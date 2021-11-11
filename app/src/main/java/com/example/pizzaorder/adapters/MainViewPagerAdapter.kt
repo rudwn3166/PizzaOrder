df32b0b047a25725918c14e811064a89a14f2170
Joo <rudwn3166@naver.com>
2021-11-11 19:50:23 +0900

    뷰페이저용 어댑터 클래스 작성

---
+++ app/src/main/java/com/example/pizzaorder/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.pizzaorder.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.pizzaorder.fragments.ChickenStoreListFragment
+import com.example.pizzaorder.fragments.PizzaStoreListFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+//       탭의 갯수가 몇개인지지
+    override fun getCount(): Int {
+
+        return 2
+
+
+//    포지션 마다 어떤 탭이 나와야하는지
+
+    }
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+
+            0->PizzaStoreListFragment()
+            else->ChickenStoreListFragment()
+
+        }
+    }
+
+}
