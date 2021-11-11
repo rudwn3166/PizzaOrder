802919f35e45fc2edb7847754a9f81da93357abf
Joo <rudwn3166@naver.com>
2021-11-11 19:29:16 +0900

    ap메인화면이 베이스액티비티 기반으로 동작하도록 함

--- app/src/main/java/com/example/pizzaorder/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorder/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.pizzaorder
 
+import android.os.BaseBundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValuse()
     }
+    override fun setupEvents() {
+//        버튼 클릭등의 이벤트 관련 코드 모음
+    }
+
+    override fun setValuse() {
+//        화면에 데이터를 표시하기 위한 코드 모음
+        
+    }
+
+  
 }
