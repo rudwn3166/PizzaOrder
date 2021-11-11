15dceea25707b5fbe85d586dfdf8ef3ae0cd710a
Joo <rudwn3166@naver.com>
2021-11-11 19:43:16 +0900

    치킨가게 목록용 프래그먼트도 추가함

---
+++ app/src/main/java/com/example/pizzaorder/fragments/ChickenStoreListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.pizzaorder.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizzaorder.R
+
+class ChickenStoreListFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+//        인플레이터는 xml을 객체화 시켜주는 기능능
+       return inflater.inflate(R.layout.fragment_chicken_store_list,container,false)
+        
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+//        동작 관련 코드
+    }
+
+}
