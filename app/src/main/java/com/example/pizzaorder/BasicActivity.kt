2b9f612c6bfc24a6ad394d7e1e12352479612e00
Joo <rudwn3166@naver.com>
2021-11-11 19:25:16 +0900

    베이스 액티비티를 추가 했다

---
+++ app/src/main/java/com/example/pizzaorder/BasicActivity.kt
@@ -0,0 +1,15 @@
+package com.example.pizzaorder
+
+import androidx.appcompat.app.AppCompatActivity
+
+//추상 클래스 abstract
+abstract class BasicActivity :AppCompatActivity() {
+
+//    디스를 미리 받아두기
+     val mContext =this
+
+
+//    존재는 하지만 명시만 하는 함수,이벤트 처리만을 위한 함수
+    abstract fun setupEvents()
+    abstract fun setValuse()
+}
