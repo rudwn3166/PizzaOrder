ae2be994348cc78d59d3aa2b40b2620f8388342e
Joo <rudwn3166@naver.com>
2021-12-02 20:34:18 +0900

    실제 데이터 추가 + 어댑터/리스트뷰 연결

--- app/src/main/java/com/example/pizzaorder/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/example/pizzaorder/fragments/PizzaStoreListFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pizzaorder.R
+import com.example.pizzaorder.adapters.PizzaStoreAdapter
+import com.example.pizzaorder.datas.Store
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment:Fragment() {
+
+    val mPizzaStoreDataList = ArrayList<Store>()
+    lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +30,15 @@ class PizzaStoreListFragment:Fragment() {
         super.onActivityCreated(savedInstanceState)
         
 //        동작 관련 코드
+
+        mPizzaStoreDataList.add(Store("B파스타","0504-829-0724","https://cdn3.iconfinder.com/data/icons/street-food-and-food-trucker-1/64/pizza-fast-food-bake-bread-128.png"))
+        mPizzaStoreDataList.add(Store("59쌀피자","0504-820-7285","https://cdn1.iconfinder.com/data/icons/cartoon-snack/128/pizza-128.png"))
+        mPizzaStoreDataList.add(Store("반올림피자샵","032-566-3334","https://cdn3.iconfinder.com/data/icons/food-set-3/91/Food_C219-128.png"))
+        mPizzaStoreDataList.add(Store("달콤팩토리","0504-920-3960","https://cdn0.iconfinder.com/data/icons/fastfood-31/64/pizza-italian-food-fast-fastfood-cheese-piece-128.png"))
+
+        mPizzaStoreAdapter=PizzaStoreAdapter(requireContext(),R.layout.pizza_store_listitem,mPizzaStoreDataList)
+        pizzaStoreListView.adapter=mPizzaStoreAdapter
+
     }
 
 }
