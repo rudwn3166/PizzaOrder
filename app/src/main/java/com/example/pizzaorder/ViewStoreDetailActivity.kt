81d187f00d659e3f2a36a0bf61b329b78f3dfb41
Joo <rudwn3166@naver.com>
2021-12-03 19:01:04 +0900

    가게 상세 보기 화면 추가

---
+++ app/src/main/java/com/example/pizzaorder/ViewStoreDetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.pizzaorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BasicActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+
+        setupEvents()
+        setValuse()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValuse() {
+
+    }
+
+
+}
